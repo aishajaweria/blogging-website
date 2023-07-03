7ec6043b2084fc4037b214785500b4315bc8f176
aishajaweria <82979914+aishajaweria@users.noreply.github.com>
2023-07-03 16:19:34 +0500

    Add files via upload

---
+++ aboutUs/aboutUs.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Us</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
+    <?php require "../partials/links.php" ?>
+	<link rel="stylesheet" type="text/css" href="../static/css/aboutUs.css">
+
+</head>	
+<body>
+    <?php 
+    require "../partials/sideBar.php";
+    ?>
+	<div class="section">
+		<div class="container">
+			<div class="content-section">
+				<h1 class="title">About us</h1>
+				<p class="text">
+				Welcome to our dynamic food blogging website, where culinary creativity and delectable flavors come to life! Here, we are passionate about sharing mouthwatering recipes, cooking tips, and all things related to the art of gastronomy.
+				Whether you're a seasoned chef or a kitchen novice, our platform is designed to inspire and guide you on your culinary journey. With a diverse range of recipes spanning various cuisines, dietary preferences, and cooking techniques, there's something here for everyone.
+Our team of dedicated food enthusiasts meticulously curates each recipe, ensuring they are easy to follow, accompanied by stunning visuals, and bursting with flavor. From savory starters and tantalizing main courses to indulgent desserts and refreshing beverages, we have you covered for every meal occasion.
+<span class="dots">.........</span>
+<span class="more-text">But we're not just about recipes. We believe that food is an experience, and we strive to create a community where food lovers can connect, learn, and share their culinary adventures. Discover insightful cooking tips, ingredient guides, and engaging articles that delve into the fascinating world of food.
+We invite you to explore our dynamic food blogging website, get inspired, and embark on a flavorful journey with us. So tie on your apron, sharpen those knives, and let's unleash your inner chef together!
+</span></p>
+              <button class="read-more-btn">Read more</button>
+				<div class="social">
+					<a href=""><i class="fab fa-facebook-f"></i></a>
+					<a href=""><i class="fab fa-twitter"></i></a>
+					<a href=""><i class="fab fa-instagram"></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script src="../static/js/aboutUs.js"></script>
+</body>
+</html>
+
+
+
+
+
